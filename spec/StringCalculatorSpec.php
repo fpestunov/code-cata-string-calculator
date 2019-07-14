0ab5bfeaae9e396394b525581c706227d0f5fca3
fpestunov <fpestunov@gmail.com>
2019-07-14 07:41:14 +0400

    init String Calculator project

---
+++ spec/StringCalculatorSpec.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace spec;
+
+use StringCalculator;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class StringCalculatorSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(StringCalculator::class);
+    }
+    
+}
